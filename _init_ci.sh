aab1fa1240e4b08ae7fd576c59fd517ad10d5148
Steffen Deusch <steffen@deusch.me>
2021-07-04 17:41:01 +0200

    rsync

--- _init_ci.sh
+++ _init_ci.sh
@@ -2,7 +2,6 @@
 
 mkdir -p ~/.ssh
 eval "$(ssh-agent)"
-echo "$NERVES_SSH_KEY" > ~/.ssh/id_rsa.pub
 echo "$NERVES_GITHUB_KEY" > ~/.ssh/github.key
 chmod 600 ~/.ssh/github.key
 ssh-add ~/.ssh/github.key
